0befc446c22033c249c55febe5786bf19eca7b8e
stampoints <stampointsrecords@gmail.com>
2025-06-02 07:48:53 +0100

    Refactor app structure and enhance employee management
    Updated `App.razor` for proper HTML structure and removed interactive server mode. Simplified `Counter.razor` by replacing the `Employees` list with a `currentCount` variable. Enhanced `DataView.razor` with error handling and modified navigation logic. Added `HttpContext` support in `Error.razor`. Removed authorization imports in `_Imports.razor`. Restored `DbContext.cs` with necessary properties. Modified `Program.cs` to change service registrations. Introduced `EditEmployee.razor` for employee detail editing.

--- TablesSQLSignInOut/DbContext.cs
+++ TablesSQLSignInOut/DbContext.cs
@@ -10,15 +10,21 @@ public class YourDbContext : DbContext
         : base(options)
     {
     }
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
-    //{
-    //   optionBuilder.UseSqlServer("Data Source= 100.117.170.73; Database = SignInOutData; User Id = ; Password = Gjholli&2201; TrustServerCertificate = True;")
-    //        .AddInterceptors(new AuditInticeptor());
-
-    //}
 
     public DbSet<Employee> Employees { get; set; }
-
     public DbSet<AuditEntity> AuditEntries { get; set; }
 }
 
+public class Employee
+{
+    public int ID { get; set; }
+    public int WorkID { get; set; }
+    public string Name { get; set; }
+    public string FamilyName { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    public string UserPage { get; set; }
+    public int WorkTimeTotal { get; set; }
+    public string JobTitle { get; set; }
+    public string privileges { get; set; }
+}
--- TablesSQLSignInOut/Program.cs
+++ TablesSQLSignInOut/Program.cs
@@ -1,17 +1,11 @@
 using TablesSQLSignInOut.Components;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
-using TablesSQLSignInOut.Database;
-using TablesSQLSignInOut.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddDbContextFactory<YourDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-builder.Services.AddScoped<DbEdit>();
 
 
 // Add services to the container.
@@ -27,7 +21,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 
